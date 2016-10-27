b54738051874051ed83804ef2dfd525cdd31027b
overgd <overgd22@naver.com>
2016-10-28 00:43:12 +0900

    161027

---
+++ app/src/main/java/com/overflow/overlab/checkcalendar/CalendarView/CalendarDayTextView.java
@@ -0,0 +1,20 @@
+package com.overflow.overlab.checkcalendar.CalendarView;
+
+import android.content.Context;
+import android.widget.TextView;
+
+/**
+ * Created by over on 10/28/2016.
+ */
+
+public class CalendarDayTextView extends TextView {
+
+    public CalendarDayTextView(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+
+    }
+}
